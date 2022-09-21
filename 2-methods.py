8bc12fbf8e103ff79071a40e7d78a23712d4c756
Berru <68827388+berrua@users.noreply.github.com>
2022-09-21 08:48:33 +0300

    Add files via upload

---
+++ 2-methods.py
@@ -0,0 +1,28 @@
+#Substring
+
+a = "hello world"
+a[0] #alt stringlere erismek icin kullanilir
+a[1] #1. indise erisildi, e
+a[0:5] #0-5 arasi karakterler secildi
+
+#Tip donusumleri
+
+toplama_bir = input() #input, kullanicidan bilgi alir
+toplama_iki = input() 
+int(toplama_bir) + int(toplama_iki) 
+
+int(11.0)
+float(12)
+str(13)
+type(str(14))
+
+#Print fonksiyonu
+
+print("Hello World")
+print("Hello", "World")
+
+#fonksiyonlarin genel amaclarini bicimlendirmek icin kullanilan alt gorev belirticilere arguman denir
+print("Hello", "World", sep = "_")
+print("Hello", "World", sep = ",")
+
+?print
