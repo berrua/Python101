9861b1d26d30a3b562489c426fbdbd9194b6f445
Berru <68827388+berrua@users.noreply.github.com>
2022-09-28 11:05:34 +0300

    Add files via upload

---
+++ 4-tuple.py
@@ -0,0 +1,14 @@
+#Tuple
+
+t = ("ali","veli",1,2,3.2,[1,2,3,4])
+
+t = "ali","veli",1,2,3.2,[1,2,3,4]
+
+t = ("eleman",) #tek elemanli tupleda "," kullanilir
+type(t)
+
+t = "ali","veli",1,2,3.2,[1,2,3,4]
+t[1]
+t[0:3]
+
+t[2] = 99 #degistirilemez
