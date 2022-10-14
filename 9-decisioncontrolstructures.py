0372b15215c1d367baa2d6a324cca39684bfebcd
Berru <68827388+berrua@users.noreply.github.com>
2022-10-14 22:50:09 +0300

    Add files via upload

---
+++ 9-decisioncontrolstructures.py
@@ -0,0 +1,39 @@
+#Decision Control Structures
+
+#True-False Sorgulari
+
+sinir = 5000
+sinir == 4000 #false
+sinir == 5000 #true
+
+#If Yapisi
+
+sinir = 50000
+gelir = 40000
+
+if gelir < sinir:
+    print("Gelir sinirdan kucuk.")
+
+if gelir > sinir:
+    print("Gelir sinirdan buyuk.")
+    
+#Else Yapisi
+
+sinir = 50000
+gelir = 60000
+
+if gelir < sinir:
+    print("Gelir sinirdan kucuk.")
+else:
+    print("Gelir sinirdan buyuk.")
+    
+sinir = 50000
+gelir = 50000
+    
+if gelir == sinir:
+    print("Gelir sinira esit.")
+else:
+    print("Gelir sinira esit degil.")
+    
+#Elif Yapisi
+
